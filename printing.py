9a4398b9b26261e0e4a72ce1cbcdbf6eed5566f4
etheleon <picy2k@gmail.com>
2016-12-14 17:00:23 +0800

    first commit

---
+++ printing.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+
+import datetime
+from collections import defaultdict
+
+class io:
+    def __init__ (self, totalReads, rootDir, sampleName, sampleDir, readInfo, outputFile):
+        self.root = rootDir
+        self.outfile = outputFile
+        self.readInfo = readInfo
+        self.heading = [
+                ('@Creator'         , 'blast2lca2megansummary'),
+                ('@CreationDate'    , datetime.datetime.utcnow().strftime("%a %b %d %H:%M:%S UTC %Y")),
+                ('@ContentType'     , 'Summary4'),
+                ('@Names'           , sampleName),
+                ('@BlastMode'       , 'BlastX'),
+                ('@Uids'            , 1480988728000), #honestly what is this file for?
+                ('@Sizes'           , totalReads),
+                ('@TotalReads'      , totalReads),
+                ('@AdditionalReads' , 0),
+                ('@Collapse'        , 'Taxonomy        -1'),
+                ('@Algorithm'       , 'Taxonomy        LCA'),
+                ('@Parameters'      , "minScore=50.0 maxExpected='0.01' minPercentIdentity='0.0' topPercent=10.0 minSupportPercent=0.01 minSupport=393 weightedLCAPercent=80.0 minComplexity=0.0 fNames= { Taxonomy KEGG }"),
+                ('@NodeStyle'       , 'Taxonomy        Circle'),
+                ('@ColorTable'      , 'Fews8   White-Green'),
+                ('@ColorEdits'      , ''),
+            ]
+        self.singleEnding = "END_OF_DATA_TABLE\n #SampleID       @Source\n %s.daa      %s/%s.daa\n" % (sampleName, sampleDir, sampleName)
+
+    def printMeganSummary(self):
+        print("Writing .megan summary file to output: %s" % self.outfile)
+        with open("%s%s" % (self.outfile, ".megan"), 'w') as outfile:
+            print("Printing header...")
+            for elem in self.heading:
+                line  = "%s\t%s" % (elem[0], elem[1])
+                outfile.write(line + "\n")
+            print("Printing taxa summary...")
+            self.__summariseTaxa(outfile)
+            print("Printing KO summary...")
+            self.__summariseKO(outfile)
+            outfile.write(self.singleEnding + "\n")
+
+    def printCombinedAnalysis(self):
+        translate = {
+            'p': 'phylum',
+            'c': 'class',
+            'o': 'order',
+            'f': 'family',
+            'g': 'genus',
+            's': 'species',
+        }
+        with open("%s%s" % (self.outfile, "-combined.txt"),'w') as outfile:
+            for rank in ['p', 'c', 'o', 'f', 'g', 's']:
+                rankDict = defaultdict(lambda: defaultdict(int))
+                for indiv in self.readInfo:
+                    try:
+                        taxa    = self.readInfo[indiv]['taxa'][rank]
+                        ko      = self.readInfo[indiv]['ko']
+                        rankDict[taxa][ko] +=1
+                    except KeyError:
+                        print("rank is not available")
+                for taxon in rankDict:
+                    for ko in rankDict[taxon]:
+                        outfile.write("%s\t%s\tK%s\t%s\n" % (translate[rank], taxon, ko, rankDict[taxon][ko]))
+
+    def __summariseTaxa(self, fh):
+        taxahash = defaultdict(lambda:1)
+        translate = {
+            'p': 'phylum',
+            'c': 'class',
+            'o': 'order',
+            'f': 'family',
+            'g': 'genus',
+            's': 'species',
+        }
+        for rank in ['p', 'c', 'o', 'f', 'g', 's']:
+            print("Taxonomy Processing: %s" % translate[rank])
+            for indiv in self.readInfo:
+                try:
+                    taxa = self.readInfo[indiv]['taxa'][rank]
+                    taxahash[taxa] +=1
+                except KeyError:
+                    print("rank is not available")
+        for taxa in taxahash:
+            line = "tax\t%s\t%s" % (taxa,taxahash[taxa])
+            fh.write(line + "\n")
+        print("%s taxa sumamrised"% len(taxahash))
+
+    def __summariseKO(self, fh):
+        kohash = defaultdict(lambda:1)
+        print("Function: KEGG - Processing...")
+        for read in self.readInfo:
+            ko = self.readInfo[read]['ko']
+            kohash[ko] += 1
+        for ko in kohash:
+            count = kohash[ko]
+            line = "KEGG\t%s\t%s" % (ko,count)
+            fh.write(line + "\n")
+        print("%s ko sumamrised"% len(kohash))
---
+++ process.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python
+
+import re
+from collections import deque, defaultdict
+
+from printing import io
+
+class Parser:
+    """
+    For processing BLAST2LCA outputs
+
+    Allows one to parse via command line MEGAN tools blast2lca outputs:
+    # Taxonomy
+        'HISEQ:327:HN35KBCXX:2:1101:17405:2046/1; ;d__2; 100;p__976; 100;c__200643; 100;o__171549; 100;f__171552; 80;g__838; 80;s__1262917; 20;'
+    # KO - KEGG
+        HISEQ:327:HN35KBCXX:2:1101:16910:2803/1; ;
+        HISEQ:327:HN35KBCXX:2:1101:16768:6954/1; ; [1] K04079: 100 # 1
+    """
+
+    def __init__ (self, rootPath, sampleDir, sampleName, kofile, taxfile):
+        #sample details
+        self.rootPath   = rootPath
+        self.sampleName = sampleName
+        self.sampleDir = sampleDir
+        self.outputFile = "%s/%s/%s" % (rootPath, sampleDir, sampleName)
+        print("outputFile: %s" % self.outputFile)
+        self.kofile     = "%s/%s/%s" % (rootPath, sampleDir, kofile)
+        print("kofile: %s" % self.kofile)
+        self.taxfile    = "%s/%s/%s" % (rootPath, sampleDir, taxfile)
+        print("taxfile: %s" % self.taxfile)
+        #data
+        self.totalReads   = 0
+        self.reads        = defaultdict(dict)
+        self.kohash       = defaultdict(lambda: 1)
+        self.taxonomyhash = defaultdict(lambda:1)
+
+    def singleComparison(self):
+        koreads     = self.__parseKO()
+        taxareads   = self.__parseTAXA()
+        if koreads == taxareads:
+            self.totalReads = koreads
+        else:
+            raise ValueError("total number of reads dont tally")
+        summary = io(
+            self.totalReads,
+            self.rootPath,
+            self.sampleName,
+            self.sampleDir,
+            self.reads,
+            self.outputFile
+        )
+        summary.printMeganSummary()
+
+    def combined(self):
+        koreads     = self.__parseKO()
+        taxareads   = self.__parseTAXA()
+        if koreads == taxareads:
+            self.totalReads = koreads
+        else:
+            raise ValueError("total number of reads dont tally")
+        summary = io(
+            self.totalReads,
+            self.rootPath,
+            self.sampleName,
+            self.sampleDir,
+            self.reads,
+            self.outputFile
+        )
+        summary.printCombinedAnalysis()
+
+    def __parseKO(self):
+        """
+        HISEQ:327:HN35KBCXX:2:1101:16910:2803/1; ;
+        HISEQ:327:HN35KBCXX:2:1101:16768:6954/1; ; [1] K04079: 100 # 1
+        """
+        print("Processing KOs....")
+        totalReads = 0
+        with open(self.kofile, 'r') as koFile:
+            for line in koFile:
+                totalReads += 1
+                elements = deque(line.split(";"))
+                readID = elements.popleft()
+                elements.popleft()
+                data = elements.popleft()
+                found = bool(re.search("K(\d{5})", data))
+                if found:
+                    ko = re.search("K(\d{5})", data).groups()[0]
+                    self.kohash[ko] += 1
+                    self.reads[readID]['ko'] = ko
+                else:
+                    self.reads[readID]['ko'] = '00000'
+        print("Total number of reads processed (ko): %s" % len(self.reads))
+        return totalReads
+
+    def __parseTAXA(self):
+        """
+        HISEQ:327:HN35KBCXX:2:1101:17405:2046/1; ;d__2; 100;p__976; 100;c__200643; 100;o__171549; 100;f__171552; 80;g__838; 80;s__1262917; 20;
+        """
+        print("Processing TAXA....")
+        totalReads = 0
+        with open(self.taxfile, 'r') as taxFile:
+            for line in taxFile:
+                phylahash = {}
+                totalReads +=1
+                elements    = deque(line.split(";"))
+                readID      = elements.popleft()
+                elements.popleft()
+                while elements:
+                    try:
+                        if len(elements) == 1 :
+                            break
+                        #what is the case?
+                        else:
+                            try:
+                                assignment = elements.popleft()
+                                rank, taxa = assignment.split("__")
+                                score = int(elements.popleft())
+                                if score < 50 :
+                                    break
+                                else:
+                                    phylahash[rank] = taxa
+                                    self.taxonomyhash[taxa] += 1
+                            except TypeError:
+                                print("this is the assignment : %s" % assignment)
+                    except IndexError:
+                        break
+                self.reads[readID]['taxa'] = phylahash
+        print("Total number of reads processed (taxonomy): %s" % len(self.reads))
+        return totalReads
